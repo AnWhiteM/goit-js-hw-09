19e5b3dd000b5e6ddb9f2cd2716919ba99f22c86
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2023-12-08 18:27:13 +0200

    1st task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let colorChangeInterval;
+
+function startColorSwitching() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitching() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(colorChangeInterval);
+}
+
+startButton.addEventListener('click', startColorSwitching);
+stopButton.addEventListener('click', stopColorSwitching);
